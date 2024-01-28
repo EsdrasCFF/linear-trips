9a46e0d4f9d0b0e1a3194a588b5bc9713817309d
EsdrasCFF <ecff.dev@gmail.com>
2024-01-27 23:38:26 -0300

    feat: add initial searchTrip

--- src/app/(home)/components/TripSearch.tsx
+++ src/app/(home)/components/TripSearch.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Button from "@/components/Button";
 import CurrencyInput from "@/components/CurrencyInput";
 import DatePicker from "@/components/DatePicker"
 import Input from "@/components/Input"
@@ -11,7 +12,7 @@ export function TripSearch() {
 
   }
   return (
-    <div className="container mx-auto p-5">
+    <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat">
       <h1 className="text-primaryDarker font-semibold text-xl text-center">
         Encontre sua próxima <span className="text-primary"> viagem!</span>
       </h1>
@@ -24,8 +25,8 @@ export function TripSearch() {
           <CurrencyInput placeholder="Orçamento?" />
         </div>
 
+        <Button>Buscar</Button>
       </div>
-
     </div>
   )
 }
