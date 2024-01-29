9031a00f93138df7686fce4dcdef20ab72ac9ee2
EsdrasCFF <ecff.dev@gmail.com>
2024-01-29 01:26:17 -0300

    feat: add TripDescripton component

---
+++ src/app/trips/[tripId]/components/TripDescription.tsx
@@ -0,0 +1,13 @@
+interface TripDescriptionProps {
+  description: string;
+}
+
+export function TripDescription({description}: TripDescriptionProps) {
+  return (
+    <div className="flex flex-col px-5" >
+      <h1 className="font-semibold text-base text-primaryDarker" >Sobre a Viagem</h1>
+    
+      <p className="text-xs leading-5 mt-1 text-primaryDarker" >{description}</p>
+    </div>
+  )
+}
