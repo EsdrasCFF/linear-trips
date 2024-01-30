0bb8170da0b60a525284505ffe7b647e34ce06ae
EsdrasCFF <ecff.dev@gmail.com>
2024-01-29 22:31:19 -0300

    feat: add migration locationDescription column

---
+++ prisma/migrations/20240130005118_add/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Trip" ADD COLUMN     "locationDescription" TEXT NOT NULL DEFAULT 'é uma encantadora cidade histórica com arquitetura colonial preservada, praias paradisíacas e uma atmosfera cultural única, perfeita para quem busca história, natureza e belas paisagens.';
