60baf9c143e816a9e03488a1cba7129724fae5bd
EsdrasCFF <ecff.dev@gmail.com>
2024-01-30 01:03:24 -0300

    feat: add react-hook-form

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -1,30 +1,86 @@
 "use client"
+
 import Button from "@/components/Button";
 import DatePicker from "@/components/DatePicker";
 import Input from "@/components/Input";
 import { Trip } from "@prisma/client";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from '@hookform/resolvers/zod'
+import { FormError } from "@/components/FormError";
 
 interface TripReservationProps {
   trip: Trip;
 }
 
+const TripReservationSchema = z.object({
+  guests: z.number().min(1, {message: 'Informe pelo menos 1 hóspede!'}).max(5, 'Informe no máximo 5 hóspedes'),
+  startDate: z.union([z.date(), z.null()]),
+  endDate: z.union([z.date(), z.null()])
+})
+
+type TripReservationFormData = z.infer<typeof TripReservationSchema>
+
 export function TripReservation({trip}:TripReservationProps) {
+
+  const {register, handleSubmit, formState: {isSubmitting, errors}, control} = useForm<TripReservationFormData>({
+    resolver: zodResolver(TripReservationSchema),
+  });
+  
+  function handleFormSubmit(data: TripReservationFormData) {
+    console.log(data)
+  }
+
   return (
     
     <div className="flex flex-col px-5" >
       <div className="flex gap-3" >
-        <DatePicker placeholderText="Data de Início" onChange={() => {}} className="w-full" />
-        <DatePicker placeholderText="Data Final" onChange={() => {}} className="w-full" />
+        <Controller
+          name="startDate"
+          rules={{ required: {value: true, message: 'Data inicial é obritatória!'}}}
+          control={control}
+          render={({field}) => (
+            <DatePicker 
+              placeholderText="Data de Início" onChange={field.onChange} className="w-full" 
+              error={!!errors.startDate}
+              errorMessage={errors.startDate?.message}
+              selected={field.value}
+            />
+          )}
+        />
+
+        <Controller
+          name="endDate"
+          rules={{ required: {value: true, message: 'Data inicial é obritatória!'}}}
+          control={control}
+          render={({field}) => (
+            <DatePicker 
+              placeholderText="Data Final" onChange={field.onChange} className="w-full" 
+              error={!!errors.endDate}
+              errorMessage={errors.endDate?.message}
+              selected={field.value}
+            />
+          )}
+        />
+        
       </div>
 
-      <Input placeholder={`Número de hospedes (max: ${trip.maxGuests})`} className="mt-[10px]"/>
+      <div>
+        <Input {...register('guests', {valueAsNumber: true})} 
+          type="number" placeholder={`Número de hospedes (max: ${trip.maxGuests})`} className="mt-[10px]"
+        />
+        
+        {errors.guests && (
+          <FormError message={errors.guests?.message!} />
+        )}
+      </div>
     
       <div className="flex justify-between mt-[10px]" >
         <p className="text-primaryDarker font-medium" >Total (7 noites)</p>
         <p className="text-primaryDarker font-medium" >R$ 2.160 </p>
       </div>
 
-      <Button className="mt-[10px]" >Reservar Agora</Button>
+      <Button className="mt-[10px]" onClick={handleSubmit(handleFormSubmit)}>Reservar Agora</Button>
 
       <div className="my-10 border text-grayLighter" ></div>
 
