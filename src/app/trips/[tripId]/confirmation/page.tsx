9846d579619e76c8b2cb7aca6935ba9f1f1e17aa
EsdrasCFF <ecff.dev@gmail.com>
2024-01-31 08:49:36 -0300

    style: finishing styled confirmation page

--- src/app/trips/[tripId]/confirmation/page.tsx
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -8,7 +8,8 @@ import Image from "next/image";
 import ReactCountryFlag from "react-country-flag";
 import { priceFormatter } from "@/utils/formatter";
 import Button from "@/components/Button";
-import prBR from 'date-fns/locale/pt-BR'
+import ptBR from 'date-fns/locale/pt-BR'
+import { format } from "date-fns";
 
 interface TripConfirmationPageProps {
   params: {
@@ -20,26 +21,34 @@ export default function TripConfirmationPage({params}: TripConfirmationPageProps
   const [trip, setTrip] = useState<Trip | null>()
   const [totalPrice, setTotalPrice] = useState<number | null>()
   
-  const seachParams = useSearchParams()
+  const searchParams = useSearchParams()
 
+  const startDateString = searchParams.get('startDate')
+  const endDateString = searchParams.get('endDate')
+  const guestsString = searchParams.get('guests')
+  
+  const tripStartDate = startDateString && format(new Date(startDateString), 'dd-MMM', {locale: ptBR})
+  const tripEndDate = endDateString && format(new Date(endDateString), 'dd-MMM', {locale: ptBR})
+  const tripGuests = guestsString && guestsString
+  
   useEffect(() => {
     const fetchTrip = async () => {
       const response = await api.post('/trips/check', {
-        startDate: seachParams.get('startDate'),
-        endDate: seachParams.get('endDate'),
+        startDate: searchParams.get('startDate'),
+        endDate: searchParams.get('endDate'),
         tripId: params.tripId
       })
-
+      
       
       setTrip(response.data.trip)
       setTotalPrice(Number(response.data.totalPrice))    
     }
-
+    
     fetchTrip()
   },[])
   
   if(!trip) return null
-
+  
   return (
     <div className="container mx-auto p-5">
       <h1 className="text-xl font-semibold text-primaryDarker" >Sua viagem</h1>
@@ -66,12 +75,12 @@ export default function TripConfirmationPage({params}: TripConfirmationPageProps
         </div>
       </div>
 
-      <div className="flex flex-col">
-        <h3 className="text-sm text-primaryDarker" >Data</h3>
-        <p>17-19 junho</p>
+      <div className="flex flex-col text-sm text-primaryDarker">
+        <h3 className="mt-5" >Data</h3>
+        <p className="mt-1" >{`${tripStartDate} a ${tripEndDate}`}</p>
       
-        <h3>Hóspedes</h3>
-        <p>8 hóspedes</p>
+        <h3 className="mt-5">Hóspedes</h3>
+        <p className="mt-1" >{tripGuests} Hóspedes</p>
       </div>
 
       <Button></Button>
